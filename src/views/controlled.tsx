1ca7739f17b5d13d8152258d934cd9b29f6aa916
loveverse <398096830@qq.com>
2022-11-07 13:59:58 +0800

    受控组件

---
+++ src/views/controlled.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import axios from "axios";
+export default class Controlled extends Component {
+  state = {
+    iptContent: "",
+    list: [],
+  };
+
+  render() {
+    return (
+      <div>
+        {/* 受控组件 */}
+        <p>{this.state.iptContent}</p>
+        <input
+          type="text"
+          value={this.state.iptContent}
+          onChange={(evt) => {
+            this.setState({
+              iptContent: evt.target.value,
+            });
+          }}
+        />
+        <button
+          onClick={() => {
+            console.log("[ this.state.iptContent ] >", this.state.iptContent);
+          }}
+        >
+          登录
+        </button>
+        <button onClick={this.getList}>获取数据</button>
+      </div>
+    );
+  }
+  getList = () => {
+    axios
+      .get("https://loveverse.top/api/findExcerpt")
+      .then((res) => {
+        this.setState({
+          list: res.data,
+        });
+        console.log("[ res ] >", this.state);
+      })
+      .catch((e) => {
+        console.log("[ e ] >", e);
+      });
+  };
+}
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,6 +3,7 @@ import { Layout } from "antd";
 import { SyncOutlined } from "@ant-design/icons";
 import Navbar from "./Navbar";
 import ClassDemo from "./class";
+import Controlled from "./controlled";
 
 function Main() {
   const [count, setCount] = useState(0);
@@ -10,11 +11,11 @@ function Main() {
   return (
     <Layout>
       <Header>
-        {/* <ClassDemo></ClassDemo> */}
         {/* <SyncOutlined /> */}
       </Header>
       {/* <SyncOutlined /> */}
       <Content onClick={() => setCount(count + 1)}>
+        <ClassDemo></ClassDemo>
         <div>
           <h2>首页</h2>
           <Navbar title="首页" />
@@ -27,6 +28,7 @@ function Main() {
           <h2>购物车</h2>
           <Navbar title="购物车" />
         </div>
+        <Controlled></Controlled>
       </Content>
       <Footer>Footer{count}</Footer>
     </Layout>
