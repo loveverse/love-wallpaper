16658fe7294b7942a33be414d8f93eac4e4a360d
loveverse <398096830@qq.com>
2022-11-16 00:16:34 +0800

    合并

--- src/views/Navbar.tsx
+++ src/views/Navbar.tsx
@@ -1,13 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, Requireable } from "react";
+import PropTypes from "prop-types";
 
 interface IProps {
   title?: string;
+  leftShow?: boolean;
+  rightShow?: boolean;
 }
 export default class Navbar extends Component<IProps> {
+  // static类属性，不需要new就可以拿到 a = 1是对象属性，需要使用new
+  // static propTypes : {
+  //   title: Requireable<string>;
+  //   leftShow: Requireable<boolean>;
+  //   rightShow: Requireable<boolean>;
+  // }; // 传参校验
+  // static defaultProps: { title: string; };
+  static propTypes = {
+    title: PropTypes.string,
+    leftShow: PropTypes.bool,
+    rightShow: PropTypes.bool,
+  };  // 属性写法
+  static defaultProps = {
+    title: "默认值"
+  }
   constructor(props: IProps) {
     super(props);
   }
   render() {
-    return <div>Navbar- {this.props.title}</div>;
+    return (
+      <div>
+        {this.props.leftShow && <button>返回</button>}
+        Navbar- {this.props.title}
+        {this.props.rightShow && <button>前进</button>}
+      </div>
+    );
   }
 }
+// 类的对象写法
+// Navbar.propTypes = {
+//   title: PropTypes.string,
+//   leftShow: PropTypes.bool,
+//   rightShow: PropTypes.bool,
+// };
+// new Navbar()
+
+// Navbar.defaultProps = {
+//   title: "默认值"
+// }
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { Layout } from "antd";
-import { SyncOutlined } from "@ant-design/icons";
+// import { SyncOutlined } from "@ant-design/icons";
 import Navbar from "./Navbar";
-import ClassDemo from "./class";
+// import ClassDemo from "./class";
 
 function Main() {
+  const obj = {
+    title: "列表",
+    leftShow: true,
+    rightShow: true,
+  };
   const [count, setCount] = useState(0);
   const { Header, Footer, Content } = Layout;
+
   return (
     <Layout>
       <Header>
@@ -17,15 +23,15 @@ function Main() {
       <Content onClick={() => setCount(count + 1)}>
         <div>
           <h2>首页</h2>
-          <Navbar title="首页" />
+          <Navbar leftShow={false} rightShow={false} />
         </div>
         <div>
           <h2>列表</h2>
-          <Navbar title="列表" />
+          <Navbar {...obj} />
         </div>
         <div>
           <h2>购物车</h2>
-          <Navbar title="购物车" />
+          <Navbar title="购物车" leftShow={true} rightShow={false} />
         </div>
       </Content>
       <Footer>Footer{count}</Footer>
